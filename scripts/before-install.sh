221f31685a0bfa23edb646e844c33aeda6319ce0
Alberto Organista Ramírez <alberto.organistarz@gmail.com>
2024-07-16 14:50:56 -0600

    fix: Update general system

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -4,17 +4,19 @@ if [[ $OSTYPE == 'linux-gnu' ]]; then
 	sudo apt update
 	sudo apt upgrade -y
 	#
-	ENV NVM_DIR /usr/local/nvm
-	ENV NODE_VERSION 20.13.1
+#	NVM_DIR=/usr/local/nvm
+#	NODE_VERSION=20.13.1
 	#
-	RUN mkdir -p $NVM_DIR && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-	RUN bash -c ". $NVM_DIR/nvm.sh && nvm install $NODE_VERSION && nvm use $NODE_VERSION"
+#	sudo mkdir -p $NVM_DIR && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+#	bash -c ". $NVM_DIR/nvm.sh && nvm install $NODE_VERSION && nvm use $NODE_VERSION"
 	#
-	RUN ln -s $NVM_DIR/versions/node/v$NODE_VERSION/bin/node /usr/bin/node
-	RUN ln -s $NVM_DIR/versions/node/v$NODE_VERSION/bin/npm /usr/bin/npm
+#	ln -s $NVM_DIR/versions/node/v$NODE_VERSION/bin/node /usr/bin/node
+#	ln -s $NVM_DIR/versions/node/v$NODE_VERSION/bin/npm /usr/bin/npm
 	#
-	sudo apt install -y python3 python3-pip
-	sudo pip3 install awscli
+	sudo apt install nodejs
+	#
+#	sudo apt install -y python3 python3-pip
+#	sudo pip3 install awscli
 	#
 	sudo addgroup www-data
 	sudo usermod -a -G www-data ubuntu && sudo usermod -a -G www-data root
